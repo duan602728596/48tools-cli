c32ea97a94068cf0023fea9206f83a7c527f7cb2
duan602728596 <duanhaochen@126.com>
2025-08-09 20:47:54 +0800

    test: 添加测试用例

--- src/utils/utils_test.go
+++ src/utils/utils_test.go
@@ -15,3 +15,15 @@ func TestTime(t *testing.T) {
 		}
 	})
 }
+
+// TestTime2 测试时间戳的格式化
+func TestTime2(t *testing.T) {
+	testName1 := "测试时间戳格式化是否正确2"
+	t.Run(testName1, func(t *testing.T) { // 子测试（可单独运行）
+		timeStr := Time2("1753969370754")
+
+		if timeStr != "20250731214250" {
+			t.Errorf("%s：%s", testName1, "时间戳格式化计算错误")
+		}
+	})
+}
