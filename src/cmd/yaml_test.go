3c2cf57e342d01fd412f7b20eb3cf7f171ef7ce4
duan602728596 <duanhaochen@126.com>
2025-08-09 15:41:20 +0800

    test: 修改测试用例

--- src/cmd/yaml_test.go
+++ src/cmd/yaml_test.go
@@ -1,11 +1,20 @@
 package cmd
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/duan602728596/48tools-cli/v2/src/utils"
 )
 
 // TestLoadYamlConfig 测试配置文件的加载
 func TestLoadYamlConfig(t *testing.T) {
+	err := utils.InitAppDir()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	testName0 := "测试配置文件加载"
 	t.Run(testName0, func(t *testing.T) { // 子测试（可单独运行）
 		config, err := LoadYamlConfig("config.example.yaml")
